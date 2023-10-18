ab0fa1b42af6659467e8f0971087bab5a8e658d1
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2023-10-18 07:21:10 +0530

    file Access done

--- app/src/main/java/in/apps/coeaibbsr/MainActivity.kt
+++ app/src/main/java/in/apps/coeaibbsr/MainActivity.kt
@@ -1,37 +1,47 @@
 package `in`.apps.coeaibbsr
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
     lateinit var  browser:WebView
+    var f_string : ValueCallback<Array<Uri>>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+
         browser = findViewById(R.id.myWebView)
-        val url = "https://apps.coeaibbsr.in/"
+        val url = "https://www.remove.bg/upload"
         browser.settings.domStorageEnabled = true
         browser.settings.javaScriptEnabled = true
         browser.settings.useWideViewPort = true
         browser.settings.loadWithOverviewMode = true
         browser.settings.allowFileAccess = true
         browser.settings.allowContentAccess=true
+        browser.settings.allowFileAccessFromFileURLs = true
+        browser.settings.allowUniversalAccessFromFileURLs =  true
+        browser.settings.useWideViewPort = true
+        browser.settings.allowContentAccess = true
+        browser.settings.databaseEnabled = true
         browser.canGoBackOrForward(30)
 
         browser.settings.userAgentString = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
         browser.settings.supportZoom()
-        browser.loadUrl(url)
 
-//        load changing urls
-
+        //        load changing urls
         browser.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
@@ -44,24 +54,24 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        browser.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                view?.evaluateJavascript(
-                    """
-                        (function() {
-                            var button = document.getElementById('input_12');
-                            button.addEventListener('click', function() {
-                                if (document.getElementById('input')) {
-                                    document.getElementById('input').click();
-                                } else {
-                                    alert('Error!!');
-                                }
-                            });
-                        })();
-                    """.trimIndent(), null
-                )
-            }   }
+        browser.loadUrl(url)
+
+        browser.webChromeClient = object : WebChromeClient(){
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                 super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+                val intent = fileChooserParams?.createIntent()
+                f_string = filePathCallback
+                if (intent != null) {
+                    startActivityForResult(intent,101)
+                }
+                return true
+            }
+        }
+
     }
     override fun onBackPressed() {
         if(browser.canGoBack()){
@@ -69,5 +79,13 @@ class MainActivity : AppCompatActivity() {
         }
         else
             super.onBackPressed()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==101){
+            f_string?.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode,data))
+            f_string = null
+        }
     }
 }
